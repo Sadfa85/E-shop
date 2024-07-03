59bf068f7baf7cb0d683995d18d20a8027f1890a
Sadfa85 <Sv25032009@gmail.com>
2024-07-03 19:16:54 +0500

    Второй вопрос вторая обнова

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from .models import Product
+
 # Create your views here.
+
 def home(request):
     products = Product.objects.all()
-    return render(request, "index.html", {'products' : products})
+    return render(request, "index.html", {
+        'products': products
+    })
 
-def view_product(request):
-    return render(request, 'product.html')
+def view_product(request, id):
+    product = Product.objects.filter(id=id).first()
+    return render(request, 'product.html', {
+        'product': product
+    })
